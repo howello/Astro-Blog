b0ec4c0013bd98c7f9621eaabe2d327e4f6ed6d4
luhao <howe_l@163.com>
2026-07-31 16:16:24 +0800

    blog-ui 添加评论图片上传地址

--- src/scripts/Comment.ts
+++ src/scripts/Comment.ts
@@ -27,8 +27,7 @@ const uploadImage = async (file: File): Promise<string> => {
   const body = new FormData();
   body.append('file', file);
   const res = await fetch(uploadURL, { method: "POST", body });
-  // const link = (await res.json())?.data?.link;
-  const link = res.url
+  const link = (await res.json())?.url;
   if (!link) throw new Error('评论图片上传失败：响应中缺少 data.link');
   return /^https?:\/\//.test(link) ? link : `${publicPrefix.replace(/\/$/, '')}/${link.replace(/^\/+/, '')}`;
 }
